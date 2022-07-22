5c19d4b13fda3b03414547542b9ad501f44de453
Ehsan G <ehsan@say.company>
2022-07-22 00:49:58 -0400

    answers

--- src/components/Accordions.tsx
+++ src/components/Accordions.tsx
@@ -4,10 +4,13 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Grid } from "@mui/material";
+import { Grid, Link } from "@mui/material";
 import TransactionsTable from "./TransactionsTable";
+import { useAppSelector } from "../redux/app/hooks";
 
 export default function Accordions() {
+  const { data } = useAppSelector((state) => state.googleQuery);
+
   const [expanded, setExpanded] = React.useState<string | false>(false);
 
   const handleChange =
@@ -40,8 +43,22 @@ export default function Accordions() {
         </AccordionSummary>
         <AccordionDetails>
           <Typography sx={{ color: "text.primary" }}>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
+            - Average time for a block: 10 min therefore 12 block for 120 min
+          </Typography>
+          <Typography sx={{ color: "text.primary" }}>
+            - Using Poisson Distribution Probabilities -
+            <Link
+              href="https://en.wikipedia.org/wiki/Poisson_distribution"
+              sx={{ color: "text.primary" }}
+            >
+              link
+            </Link>
+          </Typography>
+          <Typography sx={{ color: "text.primary" }}>
+            - 𝝀 the mean/average = 12 - 𝑲 number of events = 0
+          </Typography>
+          <Typography sx={{ color: "text.primary" }}>
+            - Answer = Every 1130 days and 6 hours
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -60,15 +77,13 @@ export default function Accordions() {
             Question 2
           </Typography>
           <Typography sx={{ color: "text.secondary" }}>
-            YHow many times has the above happened so far in the history of
+            How many times has the above happened so far in the history of
             Bitcoin?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat
-            lectus, varius pulvinar diam eros in elit. Pellentesque convallis
-            laoreet laoreet.
+          <Typography sx={{ color: "text.primary" }}>
+            The number of rows Reference table: {data.length}
           </Typography>
         </AccordionDetails>
       </Accordion>
